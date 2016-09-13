3b90f4d6d2447704a7ba670a1e7ed907dfb62fbd
Jackson Brietzke <jack.brietzke@gmail.com>
2016-09-13 14:29:18 -0400

    very basic driver done, with if/elseif statements

--- driver.cpp
+++ driver.cpp
@@ -9,28 +9,36 @@ int main()
    HugeInteger num1(blah1, true);
    HugeInteger num2(blah2, true);
    HugeInteger result;
+   bool answer;
    if (op == "+")
    {
+      result = num1 + num2;
       cout << "Plus\n";
    }else if (op == "*")
    {
+      result = num1 * num2;
       cout << "Times\n";
    }else if (op == ">=")
    {
+      answer = num1 >= num2;
       cout << "Greater\n";
    }else if (op == "<=")
    {
+      answer = num1 <= num2;
       cout << "Lesser\n";
    }else if (op == "!=")
    {
+      answer = num1 != num2;
       cout << "notEqual\n";
    }else if (op == "==")
    {
+      answer = num1 == num2;
       cout << "equal\n";
    }else
    {
       cout << "In later additions of the lab this will be available\n";
    }
+   cout << result << endl;
    return 0;
 }
 
