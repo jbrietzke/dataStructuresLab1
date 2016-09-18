f03434912976e60ec19ccd81d1848d0fd4e91e43
Jackson Brietzke <jack.brietzke@gmail.com>
2016-09-18 11:03:21 -0400

    Final commit to be submitted for class

--- test.cpp
+++ test.cpp
@@ -10,12 +10,9 @@ using namespace std;
 int main()
 {
    HugeInteger num1, num2;
-   cin >> num1 >> num2;
+   char op;
+   cin >> num1 >> op >> num2;
    HugeInteger result = num1 + num2;
-   cout << result;
-   cout << "Now lets do it again\n";
-   cin >> num1 >> num2;
-   result = num1 + num2;
    cout << result;
    return 0;
 }
